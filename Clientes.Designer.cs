f88c863a3ba93445de22f5961afa8b6e2840277e
Bruno Pimentel <101932198+BrunoPiimentell@users.noreply.github.com>
2022-11-05 16:49:31 -0300

    Add files via upload

---
+++ Clientes.Designer.cs
@@ -0,0 +1,305 @@
+﻿
+namespace AppBruno
+{
+    partial class FrmPessoa
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnInserir = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dgvPessoa = new System.Windows.Forms.DataGridView();
+            this.btnLocalizar = new System.Windows.Forms.Button();
+            this.btnFechar = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtCelular = new System.Windows.Forms.TextBox();
+            this.txtEndereco = new System.Windows.Forms.TextBox();
+            this.txtCidade = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtDN = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPessoa)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnInserir
+            // 
+            this.btnInserir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnInserir.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInserir.ForeColor = System.Drawing.Color.Chocolate;
+            this.btnInserir.Location = new System.Drawing.Point(640, 12);
+            this.btnInserir.Name = "btnInserir";
+            this.btnInserir.Size = new System.Drawing.Size(148, 50);
+            this.btnInserir.TabIndex = 0;
+            this.btnInserir.Text = "Inserir";
+            this.btnInserir.UseVisualStyleBackColor = true;
+            this.btnInserir.Click += new System.EventHandler(this.btnInserir_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ID";
+            // 
+            // dgvPessoa
+            // 
+            this.dgvPessoa.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPessoa.Location = new System.Drawing.Point(12, 305);
+            this.dgvPessoa.Name = "dgvPessoa";
+            this.dgvPessoa.Size = new System.Drawing.Size(776, 133);
+            this.dgvPessoa.TabIndex = 2;
+            // 
+            // btnLocalizar
+            // 
+            this.btnLocalizar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLocalizar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLocalizar.ForeColor = System.Drawing.Color.MediumBlue;
+            this.btnLocalizar.Location = new System.Drawing.Point(150, 12);
+            this.btnLocalizar.Name = "btnLocalizar";
+            this.btnLocalizar.Size = new System.Drawing.Size(148, 50);
+            this.btnLocalizar.TabIndex = 3;
+            this.btnLocalizar.Text = "Localizar";
+            this.btnLocalizar.UseVisualStyleBackColor = true;
+            this.btnLocalizar.Click += new System.EventHandler(this.btnLocalizar_Click);
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.BackColor = System.Drawing.Color.DarkRed;
+            this.btnFechar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnFechar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFechar.ForeColor = System.Drawing.Color.LightGray;
+            this.btnFechar.Location = new System.Drawing.Point(640, 191);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(148, 54);
+            this.btnFechar.TabIndex = 5;
+            this.btnFechar.Text = "Fechar";
+            this.btnFechar.UseVisualStyleBackColor = false;
+            this.btnFechar.Click += new System.EventHandler(this.btnFechar_Click_1);
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExcluir.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExcluir.ForeColor = System.Drawing.Color.Chocolate;
+            this.btnExcluir.Location = new System.Drawing.Point(640, 130);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(148, 55);
+            this.btnExcluir.TabIndex = 6;
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            this.btnExcluir.Click += new System.EventHandler(this.btnExcluir_Click_1);
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEditar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEditar.ForeColor = System.Drawing.Color.Chocolate;
+            this.btnEditar.Location = new System.Drawing.Point(640, 68);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(148, 56);
+            this.btnEditar.TabIndex = 7;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = true;
+            this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(9, 252);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(120, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Data de nascimento";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(9, 213);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "E-mail";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 172);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Celular";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(12, 130);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(61, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Endereço";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(12, 49);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(39, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Nome";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(12, 91);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(46, 13);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Cidade";
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(12, 26);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(100, 20);
+            this.txtId.TabIndex = 15;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(12, 229);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(100, 20);
+            this.txtEmail.TabIndex = 16;
+            // 
+            // txtCelular
+            // 
+            this.txtCelular.Location = new System.Drawing.Point(12, 191);
+            this.txtCelular.Name = "txtCelular";
+            this.txtCelular.Size = new System.Drawing.Size(100, 20);
+            this.txtCelular.TabIndex = 17;
+            // 
+            // txtEndereco
+            // 
+            this.txtEndereco.Location = new System.Drawing.Point(12, 146);
+            this.txtEndereco.Name = "txtEndereco";
+            this.txtEndereco.Size = new System.Drawing.Size(100, 20);
+            this.txtEndereco.TabIndex = 18;
+            // 
+            // txtCidade
+            // 
+            this.txtCidade.Location = new System.Drawing.Point(12, 107);
+            this.txtCidade.Name = "txtCidade";
+            this.txtCidade.Size = new System.Drawing.Size(100, 20);
+            this.txtCidade.TabIndex = 19;
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(12, 68);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(100, 20);
+            this.txtNome.TabIndex = 20;
+            // 
+            // txtDN
+            // 
+            this.txtDN.Location = new System.Drawing.Point(12, 268);
+            this.txtDN.Name = "txtDN";
+            this.txtDN.Size = new System.Drawing.Size(100, 20);
+            this.txtDN.TabIndex = 21;
+            // 
+            // FrmPessoa
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtDN);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.txtCidade);
+            this.Controls.Add(this.txtEndereco);
+            this.Controls.Add(this.txtCelular);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnEditar);
+            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.btnFechar);
+            this.Controls.Add(this.btnLocalizar);
+            this.Controls.Add(this.dgvPessoa);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnInserir);
+            this.Name = "FrmPessoa";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPessoa)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnInserir;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dgvPessoa;
+        private System.Windows.Forms.Button btnLocalizar;
+        private System.Windows.Forms.Button btnFechar;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtCelular;
+        private System.Windows.Forms.TextBox txtEndereco;
+        private System.Windows.Forms.TextBox txtCidade;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtDN;
+    }
+}
+
---
+++ Clientes.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppBruno
+{
+    public partial class FrmPessoa : Form
+    {
+        public FrmPessoa()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmPessoa_Load(object sender, EventArgs e)
+        {
+            Pessoa pessoa = new Pessoa();
+            List<Pessoa> pessoas = pessoa.listapessoa();
+            dgvPessoa.DataSource = pessoas;
+            btnEditar.Enabled = false;
+            btnExcluir.Enabled = false;
+        }
+
+        private void dgvPessoa_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = this.dgvPessoa.Rows[e.RowIndex];
+                row.Selected = true;
+                txtId.Text = row.Cells[0].Value.ToString();
+                txtNome.Text = row.Cells[1].Value.ToString();
+                txtCidade.Text = row.Cells[2].Value.ToString();
+                txtEndereco.Text = row.Cells[3].Value.ToString();
+                txtEmail.Text = row.Cells[4].Value.ToString();
+                txtDN.Text = row.Cells[5].Value.ToString();
+                txtCelular.Text = row.Cells[6].Value.ToString();
+
+            }
+            btnEditar.Enabled = true;
+            btnExcluir.Enabled = true;
+        }
+
+        private void btnLocalizar_Click(object sender, EventArgs e)
+        {
+            if (txtId.Text == "")
+            {
+                MessageBox.Show("Por favor digite um ID válido!!!");
+                this.txtId.Focus();
+            }
+            else
+            {
+                int Id = Convert.ToInt32(txtId.Text.Trim());
+                Pessoa pessoa = new Pessoa();
+                pessoa.Localizar(Id);
+                txtNome.Text = pessoa.nome;
+                txtCidade.Text = pessoa.cidade;
+                txtEndereco.Text = pessoa.endereco;
+                txtEmail.Text = pessoa.email;
+                txtDN.Text = pessoa.data;
+                txtCelular.Text = pessoa.celular;
+                btnEditar.Enabled = true;
+                btnExcluir.Enabled = true;
+            }
+        }
+
+        private void btnInserir_Click_1(object sender, EventArgs e)
+        {
+            if (txtNome.Text == "" && txtCidade.Text == "" && txtEndereco.Text == "" && txtEmail.Text == "" && txtDN.Text == "" && txtCelular.Text == "")
+            {
+                MessageBox.Show("Não existem dados para cadastrar, por favor preencha o formulário", "Sem dados", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.txtNome.Focus();
+            }
+            else
+            {
+                Pessoa pessoa = new Pessoa();
+                pessoa.Inserir(txtNome.Text, txtCidade.Text, txtEndereco.Text, txtEmail.Text, txtDN.Text, txtCelular.Text);
+                MessageBox.Show("Pessoa cadastrada com sucesso!");
+                List<Pessoa> pessoas = pessoa.listapessoa();
+                dgvPessoa.DataSource = pessoas;
+                txtId.Text = "";
+                txtNome.Text = "";
+                txtCidade.Text = "";
+                txtEndereco.Text = "";
+                txtEmail.Text = "";
+                txtDN.Text = "";
+                txtCelular.Text = "";
+                this.txtNome.Focus();
+            }
+        }
+
+        private void btnEditar_Click_1(object sender, EventArgs e)
+        {
+            int Id = Convert.ToInt32(txtId.Text.Trim());
+            Pessoa pessoa = new Pessoa();
+            pessoa.Atualizar(Id, txtNome.Text, txtCidade.Text, txtEndereco.Text, txtEmail.Text, txtDN.Text, txtCelular.Text);
+            MessageBox.Show("Pessoa atualizada com sucesso!");
+            List<Pessoa> pessoas = pessoa.listapessoa();
+            dgvPessoa.DataSource = pessoas;
+            txtId.Text = "";
+            txtNome.Text = "";
+            txtCidade.Text = "";
+            txtEndereco.Text = "";
+            txtEmail.Text = "";
+            txtDN.Text = "";
+            txtCelular.Text = "";
+            btnEditar.Enabled = false;
+            btnExcluir.Enabled = false;
+            this.txtNome.Focus();
+        }
+
+        private void btnExcluir_Click_1(object sender, EventArgs e)
+        {
+            int Id = Convert.ToInt32(txtId.Text.Trim());
+            Pessoa pessoa = new Pessoa();
+            pessoa.Excluir(Id);
+            MessageBox.Show("Pessoa excluída com sucesso!");
+            List<Pessoa> pessoas = pessoa.listapessoa();
+            dgvPessoa.DataSource = pessoas;
+            txtId.Text = "";
+            txtNome.Text = "";
+            txtCidade.Text = "";
+            txtEndereco.Text = "";
+            txtEmail.Text = "";
+            txtDN.Text = "";
+            txtCelular.Text = "";
+            btnEditar.Enabled = false;
+            btnExcluir.Enabled = false;
+            this.txtNome.Focus();
+        }
+
+        private void btnFechar_Click_1(object sender, EventArgs e)
+        {
+            DialogResult dialog = new DialogResult();
+            dialog = MessageBox.Show("Deseja realmente sair", "Sair do aplicativo", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation);
+            if (dialog == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+    }
+}
